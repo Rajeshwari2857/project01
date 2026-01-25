363cf21f4402bdf7b4c064e5f0acaaf3dcb90ef5
Rajeshwariba Vaghela <r.vaghela2857@gmail.com>
2026-01-25 10:36:04 +0530

    packages

--- app.py
+++ app.py
@@ -8,47 +8,18 @@ app.config["SECRET_KEY"] = "8919bbe3a614d041a1bd23ad5b8b3949"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username=db.Column(db.String(20), unique=True, nullable=False)
-    email=db.Column(db.String(20), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")  
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship("Post", backref="author", lazy=True)
-    
-    def __repr__(self):
-        return f"User('{self.username}, {self.email}, {self.image_file})"
-    
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
 
-    def __repr__ (self):
-        return f'Post("{self.title}", "{self.posted}")'
-
-
-posts = [
-    {
-        'author': 'corey schafer',
-        'title': 'blog post'
-    },
-    { 
-        'author': 'johnny',
-        'title': 'blog post 2'
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html", posts = posts)
 
+
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -58,6 +29,7 @@ def register():
         return redirect(url_for('home'))
     return render_template("register.html", title="Register", form=form)
  
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
@@ -66,9 +38,10 @@ def login():
             flash('You have been logged in successfully!', 'success')
             return redirect(url_for('home'))
         else:
-            flash('login failed. Please check your email and password', 'danger')
+            flash('Login failed. Please check your email and password', 'danger')
     return render_template("login.html", title="Login", form=form)
  
+
 if __name__ == "__main__":
     app.run(debug=True)
     
